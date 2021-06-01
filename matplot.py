1d4ae2ee7ce1c5e74277f14af6eb5efa04225f4a
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2021-06-01 22:17:25 +0200

    save option

--- matplot.py
+++ matplot.py
@@ -50,6 +50,7 @@ interactive_options.add_option("--tail", dest="tail", default=None, type='int',
                                help="Plot only the last N lines in interactive plotting",
                                metavar="N")
 parser.add_option_group(interactive_options)
+parser.add_option("--save", help="Save the file", type=str, dest='outfilename')
 parser.add_option("-d", "--delimiter", help="Delimiter to use to read the data", default=None)
 parser.add_option("--mpld3" , dest="mpld3", default=False, action="store_true",
                     help="plot in the browser using mpld3 library")
@@ -529,7 +530,10 @@ def do_plot(x, y, z=None, e=None, histogram=options.histogram, scatter=options.s
             mpld3.show()
             mpld3.save_html(plt.gcf(), 'mpld3.html')
         else:
-            plt.show()
+            if options.outfilename is None:
+                plt.show()
+            else:
+                plt.savefig(options.outfilename)
 
 data = None
 while True:
