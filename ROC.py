9658b84a9c9e775f968ad6fc6c65c6c07dea6a8e
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-01-27 09:26:08 +0100

    modified:   ROC.py

--- ROC.py
+++ ROC.py
@@ -117,9 +117,11 @@ if __name__ == '__main__':
     # logging.info(f"################ Starting {__file__} ################")
     # ### ##################### ####
     # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(
+        description=
+        'Compute a ROC curve from the given data. Two columns separated by a comma must be given in stdin (using a pipe). The first column gives the negative values, the second the positive values.'
+    )
     # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
-    parser.add_argument('-a', '--arg1')
     parser.add_argument('--test', help='Test the code', action='store_true')
     parser.add_argument('--func', help='Test only the given function(s)', nargs='+')
     args = parser.parse_args()
@@ -139,3 +141,11 @@ if __name__ == '__main__':
                                                globals(),
                                                optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE)
         sys.exit()
+    data = np.genfromtxt(sys.stdin, invalid_raise=False, delimiter=',')
+    negatives = data[:, 0]
+    positives = data[:, 1]
+    x, y, auc = ROC(positives, negatives)
+    print(f'#AUC: {auc:.2f}')
+    print('#FPR #TPR')
+    for xval, yval in zip(x, y):
+        print(f'{xval:.2f} {yval:.2f}')
