29eb26adf59f026ecfe5e9b19bbab28877126211
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-09-18 14:05:45 +0200

    modified:   matplot.py

--- matplot.py
+++ matplot.py
@@ -1059,6 +1059,11 @@ def do_plot(
                             coldict = dict(zip(keys, mesh[1].flatten()))
                             keys = list(rowdict.keys())
                             sel = numpy.isin(z[:, 0], keys)
+                            print(sizes)
+                            if len(sizes) == 0:
+                                s = numpy.ones_like(x[:, 0][sel]) * options.size
+                            else:
+                                s = numpy.asarray(sizes)[sel] * options.size
                             g = sns.FacetGrid(
                                 pd.DataFrame(
                                     {
@@ -1072,13 +1077,13 @@ def do_plot(
                                 row="row",
                                 col="col",
                                 hue="z",
+                                hue_kws={"s": s},
                                 palette=options.cmap,
                             )
                             g = g.map(
                                 plt.scatter,
                                 "x",
                                 "y",
-                                s=options.size,
                                 alpha=options.alpha,
                             )
                         else:
